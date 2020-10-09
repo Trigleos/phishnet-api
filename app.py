051448047555ba02dfde71af519534607452a6aa
Henri <irneha@hotmail.com>
2020-10-09 23:19:10 +0200

    Fixed indentation

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	with open("content/index.html") as f:
-    	return f.read()
+		return f.read()
 
 @app.route("/network")
 def network():
 	with open("content/network.html") as f:
-    	return f.read()
+		return f.read()
