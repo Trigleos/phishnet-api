ec981f6cb29be8c838d61bf480b7610280d13ddc
Gary Cornelius <garycorn90@gmail.com>
2020-10-09 21:59:57 +0200

    Changes for Azure.

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>Hello Azure!</h1>"
+    return "<h1>Phishnet Archive</h1><p>This site is a prototype API for the Phishnet GameOfCode project.</p>"
